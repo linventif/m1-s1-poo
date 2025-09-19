5627b3c4ffb82bb4015b39e264f0d7cc68f990fa
linventif <gregoire.becue@pm.me>
2025-09-19 14:34:24 +0200

    fix: use AppProgImpJava insted of main

---
+++ tp01/src/main/java/launaybecue_gregoire/tp1/AppProgImpJava.java
@@ -0,0 +1,23 @@
+package launaybecue_gregoire.tp1;
+
+public class AppProgImpJava {
+    /*
+     * Zone pour les sous-programmes.
+     */
+
+    /**
+     * Retourne si un nombre entier est diviseur d’un autre.
+     */
+    public static boolean estDiviseur(int nb, int diviseur) {
+        return nb % diviseur == 0;
+    }
+
+    /*
+     * Programme principal.
+     */
+    public static void main(String[] args) {
+        /*----- Zone pour tester vos sous-programmes -----*/
+
+    }
+
+} /*----- Fin de la classe AppProgImpJava -----*/
--- tp01/src/main/java/launaybecue_gregoire/tp1/Main.java
+++
@@ -1,7 +0,0 @@
-package launaybecue_gregoire.tp1;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
